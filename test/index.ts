d15c6b243aaf987f8572fea406602ec2f4397ff0
tmp <tmp@tmp.support.com>
2022-01-21 18:43:04 +0800

    add table to show deployed info

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,32 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+const Table = require("cli-table3");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+//const UniswapV3Deployer_1 = require("../node_modules/uniswap-v3-deploy-plugin/dist/deployer/UniswapV3Deployer");
+//let v3Contracts = await UniswapV3Deployer_1.UniswapV3Deployer.deploy(deployer);
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+import { UniswapV3Deployer } from "../node_modules/uniswap-v3-deploy-plugin/dist/deployer/UniswapV3Deployer";
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+describe("Uniswap v3", function () {
+  it("deploy info", async function () {
+    let [deployer] = await ethers.getSigners();
+    let contracts = await UniswapV3Deployer.deploy(deployer);
+    //console.log("uniswap v3 contracts:");
+    //console.log("WETH9:", contracts.weth9.address);
+    //console.log("factory:", contracts.factory.address);
+    //console.log("router:", contracts.router.address);
+    //console.log("nftDescriptorLibrary:", contracts.nftDescriptorLibrary.address);
+    //console.log("positionDescriptor:", contracts.positionDescriptor.address);
+    //console.log("positionManager:", contracts.positionManager.address);
+    let table = new Table({
+          head: ["Contract", "Address"],
+          style: { border: [] },
+    });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    for (const item of Object.keys(contracts)) {
+        table.push([item, contracts[item].address]);
+    }
+    console.info(table.toString());
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
